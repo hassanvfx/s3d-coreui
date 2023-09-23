9d47cc2f24f903d61339b5982e2af0dcf5802d21
hassan <hassanvfx@gmail.com>
2023-09-23 16:14:15 -0600

    initial commit

---
+++ DemoApp/DemoApp/DemoAppApp.swift
@@ -0,0 +1,26 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by hassan uriostegui on 8/30/22.
+//
+
+import SwiftUI
+import S3DCoreUI
+
+class Model:ObservableObject{
+    init(){
+        let framework = S3DCoreUI()
+        framework.test()
+    }
+}
+
+@main
+struct DemoAppApp: App {
+    @ObservedObject var model = Model()
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sources/S3DCoreUI/S3DCoreUI.swift
@@ -0,0 +1,10 @@
+public struct S3DCoreUI {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public func test(){
+        print("S3DCoreUILib linked!")
+    }
+}
