e162e3684e5ffded50c3b96d9dd26a8cc51e11ee
hassan <hassanvfx@gmail.com>
2023-09-23 16:32:31 -0600

    first commit

--- Sources/S3DCoreUI/S3DCoreUI.swift
+++ Sources/S3DCoreUI/S3DCoreUI.swift
@@ -6,6 +6,12 @@ public struct Style {
     public var cornerRadius=12
     public var lightPalette=Palette(background: .white)
     public var darkPalette=Palette(background:.black)
+    public init(padding: Int = 8, cornerRadius: Int = 12, lightPalette: Palette = Palette(background: .white), darkPalette: Palette = Palette(background:.black)) {
+        self.padding = padding
+        self.cornerRadius = cornerRadius
+        self.lightPalette = lightPalette
+        self.darkPalette = darkPalette
+    }
     public var palette:Palette{
         isDarkMode ? darkPalette : lightPalette
     }
@@ -16,8 +22,16 @@ public struct Palette{
     public var primary=Color(UIColor.purple)
     public var secondary=Color(UIColor.red)
     public var tertiary=Color(UIColor.green)
+    public init(background: SwiftUI.Color = Color.white, primary: Color = Color(UIColor.purple), secondary: Color = Color(UIColor.red), tertiary: Color = Color(UIColor.green)) {
+        self.background = background
+        self.primary = primary
+        self.secondary = secondary
+        self.tertiary = tertiary
+    }
 }
-
+extension Style {
+    static var spec = Style()
+}
 extension Style {
     public var isDarkMode: Bool {
         if let window = UIApplication.shared.windows.first {
